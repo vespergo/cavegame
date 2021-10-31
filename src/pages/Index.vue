1a564779be0f6fc6111b6877c75efea6c445a731
Josh O'Bryan <vespergostorage@hotmail.com>
2021-10-31 15:27:35 -0500

    baseline

---
+++ src/pages/Index.vue
@@ -0,0 +1,44 @@
+<template>
+  <Layout>
+    <g-image src="~/img/cave.jpg" width="760" />
+
+    <div class="title">
+      <h1>CaveGame</h1>
+      <p class="play-mode">
+        Survival Mode
+      </p>
+      <p class="play-mode">
+        Creative Mode
+      </p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.title {
+  color: white;
+  text-align: center;
+}
+.title {
+  position: relative;
+  top: -400px;
+  left: 20p;
+}
+.home-links a {
+  margin-right: 1rem;
+}
+.play-mode {
+  margin: 10px 20px;
+  padding: 5px;
+  border: 1px solid white;
+  font-size: 20px;
+}
+</style>
