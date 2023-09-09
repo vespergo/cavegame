aea0c2169a2ca74c254ca3b39ba78baa1f2f83cb
Josh O'Bryan <vespergo@gmail.com>
2023-09-09 11:24:33 -0500

    adding textures

--- src/game.js
+++ src/game.js
@@ -20,7 +20,9 @@
 
 var game = new Phaser.Game(config);
 var player;
-var blocks = [];
+let cursors;
+let jumpKey;
+let blocks = [];
 
 function preload() {
     this.load.image('marble', '/img/blocks/marble.png');
@@ -37,22 +39,48 @@ function preload() {
 }
 
 function create() {
+
     this.add.image(config.width - 48, 30, 'sun');
+
     player = this.physics.add.sprite(30, config.height - 48, 'player');
     player.setCollideWorldBounds(true);   
+    this.physics.world.gravity.y = 800;
 
+    cursors = this.input.keyboard.addKeys({
+        left: Phaser.Input.Keyboard.KeyCodes.A,
+        right: Phaser.Input.Keyboard.KeyCodes.D
+    });
 
-    this.input.on('pointerdown', function (pointer) {        
+    jumpKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+       
+
+    // tile placement
+    this.input.on('pointerdown', function (pointer) {
         let block = this.physics.add.sprite(pointer.x, pointer.y, sessionStorage['currentTile']);
-        block.setCollideWorldBounds(true);
+        block.setCollideWorldBounds(true);        
+
+        // Enable collision between the player and the blocks
+        this.physics.add.collider(player, block);                
         blocks.push(block);
     }, this);
+
+    
 }
 
 function update()
 {
-    for (var i = 0; i < blocks.length; i++) {
-        console.log(blocks[i].x);
+    // Horizontal movement
+    if (cursors.left.isDown) {
+        player.setVelocityX(-200); // Move left
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(200); // Move right
+    } else {
+        player.setVelocityX(0); // Stop horizontal movement
+    }
+
+    // Jumping mechanics
+    if (jumpKey.isDown && player.body.onFloor()) {
+        player.setVelocityY(-400); // Jump when on the ground
     }
     
 }
